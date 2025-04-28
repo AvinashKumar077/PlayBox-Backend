d6e1fd93e3dbab4fa1d7728e4ec026e3823c4ed4
Avinash Kumar <94068118+AvinashKumar077@users.noreply.github.com>
2025-04-28 21:51:36 +0530

    MODIFIED: Ensure username is stored in lowercase during user registration and update user schema to enforce lowercase uniqueness

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -76,7 +76,7 @@ const registerUser = asyncHandler(async (req, res) => {
         coverImage: coverImage?.url || "",
         email,
         password,
-        username: username
+        username: username.toLowerCase()
     })
 
 
