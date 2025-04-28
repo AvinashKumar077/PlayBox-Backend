62ae142ae538233b6690493b2883fd6c403ef72c
Avinash Kumar <94068118+AvinashKumar077@users.noreply.github.com>
2025-04-28 22:20:20 +0530

    Add error handling middleware to manage API errors consistently

---
+++ src/middlewares/api.error.middleware.js
@@ -0,0 +1,19 @@
+import { ApiError } from "../utils/ApiError.js";
+
+const errorMiddleware = (err, req, res, next) => {
+    let statusCode = err.statusCode || 500;
+    let message = err.message || "Internal Server Error";
+
+    if (!(err instanceof ApiError)) {
+        console.error("Unexpected Error:", err);
+    }
+
+    return res.status(statusCode).json({
+        success: false,
+        message,
+        errors: err.errors || [],
+        data: null,
+    });
+};
+
+export { errorMiddleware };
